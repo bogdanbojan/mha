3a1d21e7fdb880978c8a0dcb219c68eb1e05e42d
Bogdan Bojan <bogdanbojan03@gmail.com>
2024-03-03 19:27:30 +0200

    Add the ok service

---
+++ cmd/ok/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/caarlos0/env/v6"
+)
+
+type envVars struct {
+	Port string `env:"PORT" envDefault:"8080"`
+}
+
+type service struct {
+	log *log.Logger
+	envVars
+}
+
+func (s *service) ok(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+    s.log.Println("Served request succesfully")
+}
+
+func main() {
+	envVars := envVars{}
+	if err := env.Parse(&envVars); err != nil {
+		log.Fatalf("failed to parse env vars: %v", err)
+	}
+	log := log.New(os.Stderr, "[ok] ", log.LstdFlags)
+
+	s := service{
+		envVars: envVars,
+		log:     log,
+	}
+
+	http.HandleFunc("/ok", s.ok)
+	s.log.Println("Started on port", s.Port)
+
+	err := http.ListenAndServe(":"+s.Port, nil)
+	if err != nil {
+		s.log.Fatal(err)
+	}
+}
