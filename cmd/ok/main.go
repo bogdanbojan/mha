9d8b0f7d6aa4623ab1c3ea3ebc87fd98e33347dd
Bogdan Bojan <bogdanbojan03@gmail.com>
2024-03-09 23:57:11 +0200

    Refactor ok functions and documentation

--- cmd/ok/main.go
+++ cmd/ok/main.go
@@ -8,18 +8,21 @@ import (
 	"github.com/caarlos0/env/v6"
 )
 
+// envVars represents the environment variables of the application.
 type envVars struct {
 	Port string `env:"PORT" envDefault:"8081"`
 }
 
+// service represents the main application.
 type service struct {
 	log *log.Logger
 	envVars
 }
 
+// ok is a Handler function which responds with an OK Status.
 func (s *service) ok(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-    s.log.Println("Served request succesfully")
+	s.log.Println("Served request succesfully")
 }
 
 func main() {
