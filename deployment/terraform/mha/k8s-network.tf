831509a18d890e28bab466bd8c0b0d84b2b38e0f
Bogdan Bojan <bogdanbojan03@gmail.com>
2024-03-09 23:48:15 +0200

    Add network policy in tf deployment

---
+++ deployment/terraform/mha/k8s-network.tf
@@ -0,0 +1,40 @@
+resource "kubernetes_manifest" "networkpolicy_access_nginx" {
+  manifest = {
+    "apiVersion" = "networking.k8s.io/v1"
+    "kind" = "NetworkPolicy"
+    "metadata" = {
+      "name" = "access-nginx"
+      "namespace" = "default"
+    }
+    "spec" = {
+      "egress" = [
+        {
+          "from" = [
+            {
+              "podSelector" = {
+                "matchLabels" = {
+                  "app.kubernetes.io/name" = "nginx-ingress-controller"
+                }
+              }
+            },
+          ]
+        },
+      ]
+      "ingress" = [
+        {
+          "from" = [
+            {
+              "podSelector" = {
+                "matchLabels" = {
+                  "app.kubernetes.io/name" = "nginx-ingress-controller"
+                }
+              }
+            },
+          ]
+        },
+      ]
+      "podSelector" = {}
+    }
+  }
+}
+
