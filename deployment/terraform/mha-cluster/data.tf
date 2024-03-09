94d4199ab29ab3d7712b3b48b1e056e08edcf742
Bogdan Bojan <bogdanbojan03@gmail.com>
2024-03-09 18:50:54 +0200

    Restructure tf cluster deployment files

---
+++ deployment/terraform/mha-cluster/data.tf
@@ -0,0 +1,13 @@
+# Filter out local zones, which are not currently supported 
+# with managed node groups
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
--- deployment/terraform/mha-cluster/variables.tf
+++ deployment/terraform/mha-cluster/variables.tf
@@ -3,3 +3,15 @@ variable "region" {
   type        = string
   default     = "eu-west-3"
 }
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket. Must be globally unique."
+  type        = string
+  default = "mha-tf-state"
+}
+
+variable "table_name" {
+  description = "The name of the DynamoDB table. Must be unique in this AWS account."
+  type        = string
+  default = "mha-tf-state"
+}
